270b586b6b97dec7439f9e0cd42441b8a8b9c887
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2021-10-29 00:08:56 -0400

    order update
    need to sort timestamps

--- nft.py
+++ nft.py
@@ -1,3 +1,4 @@
+from os import write
 from requests.models import Response
 import streamlit as st
 import requests
@@ -10,6 +11,7 @@ st.sidebar.subheader("Filters")
 collections = st.sidebar.text_input("Collection")
 owner_address = st.sidebar.text_input("Owner Address")
 project_address =  st.sidebar.text_input("Project Address")
+token_id = st.sidebar.text_input("Token ID(s)")
 
 
 my_address= '0x90f14e3282977416286085e0d90210A400bEFD22'
@@ -28,7 +30,9 @@ if endpoint == "Assets":
     if owner_address:
         params_assets['owner'] = owner_address
     if project_address:
-        params_orders['asset_contract_address']
+        params_orders['asset_contract_address'] = project_address
+    if token_id:
+        params_orders['token_id'] = token_id
     
 
     r = requests.get('https://api.opensea.io/api/v1/assets', params = params_assets)
@@ -36,15 +40,39 @@ if endpoint == "Assets":
     response_assets = r.json()
     response_orders = o.json()
 
-    for asset in response_assets['assets']:
-        if asset['name']:
-            st.write(asset['name'])
-        else:
-            st.write(f"{asset['collection']['name']} # {asset['token_id']}")
-        if asset['image_url'].endswith('mp4'):
-            st.video(asset['image_url'])
-        else:
-            st.image(asset['image_url'])
+    if len(params_orders)==0:
+        for asset in response_assets['assets']:
+            if asset['name']:
+                st.write(asset['name'])
+            else:
+                st.write(f"{asset['collection']['name']} # {asset['token_id']}")
+            if asset['image_url'].endswith('mp4'):
+                st.video(asset['image_url'])
+            else:
+                st.image(asset['image_url'])
+    else:
+        # created_date = []
+        # for order in response_orders['orders']:
+        #     created_date.append(response_orders['orders']['created_date'])
+
+        st.write(response_orders)
+        for order in response_orders['orders']:
+            if order['asset']['name']:
+                st.write(order['asset']['name'])
+            else:
+                st.write(f"{order['asset']['name']} # {order['asset']['token_id']}")
+           
+            #checking for current price            
+            if order['current_price']:
+                current_price = float(order['current_price'])/(10**18)
+                st.write(current_price)
+            else:
+                st.write("No current Price")
+
+            if order['asset']['image_url'].endswith('mp4'):
+                st.video(order['asset']['image_url'])
+            else:
+                st.image(order['asset']['image_url'])
 
 
     st.write(r.json())
